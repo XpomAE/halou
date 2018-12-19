8c1c845b8ed78d73ddeb32dab64eabbbe29a11b6
XpomAE <46003393+XpomAE@users.noreply.github.com>
2018-12-19 20:19:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '717587063';
+const token = '717587063:AAEx24bfXYLVZHVp7z0fa8AA9lk82FpZnD8';
 
 const options = {
 webHook: {
@@ -11,3 +11,7 @@ const bot = new TelegramBot(token, options);
 const url = "https://halou.herokuapp.com/:443";
 
 bot.setWebHook(${url}/bot${token});
+
+bot.onText(/\/test/, function (msg) {
+	bot.sendMessage(msg.chat.id,'Сам себя потести, уебок');
+});
