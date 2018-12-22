6d72ad0a2a420e51c031f23936a39c97d7a82021
XpomAE <46003393+XpomAE@users.noreply.github.com>
2018-12-22 09:51:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -44,10 +44,10 @@ bot.on('message', function (msg) {
 bot.onText(/\/ebu_sobak/, function (msg) {
 	if(msg.from.username != 'Kawaii_Ghost'){
 	bot.sendMessage(msg.chat.id,'Я ебу собак');
-	setTimeout(function () {bot.sendMessage(msg.chat.id,'Всегда готов');},1);
-	setTimeout(function () {bot.sendMessage(msg.chat.id,'Сразу трахнуть несколько котов');},2);
-	setTimeout(function () {bot.sendMessage(msg.chat.id,'Да, я зоофил не говори');},3);
-	setTimeout(function () {bot.sendMessage(msg.chat.id,'Лучше мне собачку подари');},4);
+	setTimeout(function () {bot.sendMessage(msg.chat.id,'Всегда готов');},2);
+	setTimeout(function () {bot.sendMessage(msg.chat.id,'Сразу трахнуть несколько котов');},4);
+	setTimeout(function () {bot.sendMessage(msg.chat.id,'Да, я зоофил не говори');},6);
+	setTimeout(function () {bot.sendMessage(msg.chat.id,'Лучше мне собачку подари');},8);
 	}
 });
 
